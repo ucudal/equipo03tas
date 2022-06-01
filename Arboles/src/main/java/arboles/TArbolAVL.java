b7f84be323634d836d008b47c869fbc97308e16e
franpereira <56894200+franpereira@users.noreply.github.com>
2022-06-01 20:46:30 -0300

    Factor de Equilibrio AVL

---
+++ Arboles/src/main/java/arboles/TArbolAVL.java
@@ -0,0 +1,10 @@
+package arboles;
+
+public class TArbolAVL<T> extends ArbolBB<T> {
+    public int factorEquilibrio(){
+        if (getRaiz() == null){
+            return 0;
+        }
+        return ( (TElementoAVL<T>)getRaiz()).factorEquilibrio();
+    }
+}
---
+++ Arboles/src/main/java/arboles/TElementoAVL.java
@@ -0,0 +1,26 @@
+package arboles;
+
+public class TElementoAVL<T> extends ElementoAB<T> {
+
+    public TElementoAVL(Comparable unaEtiqueta, T dato) {
+        super(unaEtiqueta, dato);
+    }
+
+    private int factorEq;
+
+    public int factorEquilibrio(){
+        int alturaIzq = 0;
+        int alturaDer = 0;
+        if (getHijoIzq() == null && getHijoDer() == null){
+            return 0;
+        }
+        if (getHijoIzq() != null) {
+            alturaIzq = ((TElementoAVL<T>) getHijoIzq()).factorEquilibrio();
+        }
+        if (getHijoDer() != null) {
+            alturaDer = ((TElementoAVL<T>) getHijoDer()).factorEquilibrio();
+        }
+        factorEq = alturaDer - alturaIzq;
+        return Math.max(alturaDer, alturaIzq) + 1;
+    }
+}
